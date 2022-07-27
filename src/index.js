e0d1360202487ebd12518c573a5775b41e7bf55e
Angel Eduardo <40308240+Anedu91@users.noreply.github.com>
2022-07-27 17:00:02 -0300

    removing json lock

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import amplitude from 'amplitude-js';
+import React from "react";
+import amplitude from "amplitude-js";
 import { render } from "react-dom";
 import { App } from "./components/App.js";
 import "./styles/index.css";
@@ -10,45 +10,43 @@ import { dirtyFixConnectWalletUI } from "./utils";
 
 // Anonymous analytics for Buildship to know how many users
 const sendAnonymousAnalytics = () => {
-    if (window.location.host.includes("localhost"))
-        return
+  if (window.location.host.includes("localhost")) return;
 
-    try {
-        amplitude.getInstance().init("e2aff4bf282355999267edb40276222a")
-        amplitude.getInstance().setOptOut(window.DEFAULTS?.analyticsOptOut ?? false)
-        amplitude.getInstance().logEvent("Opened minting website", {
-            "url": window.location.host.replace('www.', '')
-        })
-        amplitude.getInstance().setDomain(window.location.host.replace('www.', ''))
-    } catch (e) {
-        console.log("Error in sendAnonymousAnalytics()")
-    }
-}
+  try {
+    amplitude.getInstance().init("e2aff4bf282355999267edb40276222a");
+    amplitude
+      .getInstance()
+      .setOptOut(window.DEFAULTS?.analyticsOptOut ?? false);
+    amplitude.getInstance().logEvent("Opened minting website", {
+      url: window.location.host.replace("www.", ""),
+    });
+    amplitude.getInstance().setDomain(window.location.host.replace("www.", ""));
+  } catch (e) {
+    console.log("Error in sendAnonymousAnalytics()");
+  }
+};
 
 const createDOMElement = () => {
-    const body = document.getElementsByTagName('body')[0];
-    const div = Object.assign(document.createElement('div'), {
-        id: "root",
-    });
-    body.appendChild(div);
-    return div;
-}
+  const body = document.getElementsByTagName("body")[0];
+  const div = Object.assign(document.createElement("div"), {
+    id: "root",
+  });
+  body.appendChild(div);
+  return div;
+};
 
 const renderAppContainer = () => {
-    render(<App />, createDOMElement());
-}
+  render(<App />, createDOMElement());
+};
 
 renderAppContainer();
 document.addEventListener("DOMContentLoaded", () => {
-    init()
+  init();
 
-    // TODO: remove this when migrated to @buildship/web3-login or forked Web3Modal
-    // Puts "custom-metamask" provider as the first option
-    dirtyFixConnectWalletUI()
-    sendAnonymousAnalytics()
+  // TODO: remove this when migrated to @buildship/web3-login or forked Web3Modal
+  // Puts "custom-metamask" provider as the first option
+  dirtyFixConnectWalletUI();
+  sendAnonymousAnalytics();
 });
 
 export { showAlert, showMintModal, renderAppContainer };
-
-
-
